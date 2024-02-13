0ed30d1a5babc0b862405601f45d3c1c88ddfdf9
DimasSotin <sotindima@gmail.com>
2024-02-13 13:48:38 +0200

    sum

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,10 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  formEl: document.querySelector('form'),
+  inputEl: document.querySelector('input-search'),
+  btnEl: document.querySelector('btn-search'),
+};
